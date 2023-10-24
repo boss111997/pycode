e120512fa6244f576b0346e80b53b2bc08c6a429
karimulla basha <121392082+boss111997@users.noreply.github.com>
2023-10-24 18:02:36 +0530

    Add files via upload

---
+++ python if else.py
@@ -0,0 +1,9 @@
+a=int(input("score percentage:"))
+if(a>=70):
+    name=input("enter your name")
+    age=input("enter your age")
+    location=input("enter your location")
+    print("you are eligible")
+else:
+    print("you are not eligible")
+    
